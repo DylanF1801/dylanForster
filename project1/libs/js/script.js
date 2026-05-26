7120081abd03fbde9a7f6809cbee8e568af53a75
Dylan Forster <forster.dylan@hotmail.com>
2026-05-26 18:18:08 +0100

    Cleaned up code and added user location tracker

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -21,10 +21,16 @@ var basemaps = {
   "Satellite": satellite
 };
 
+/*document.querySelectorAll('.modal').forEach((modal) => {
+  modal.addEventListener('hide.bs.modal', () => {
+    document.activeElement.blur();
+  });
+});*/
+
 // buttons
 
-var infoBtn = L.easyButton("fa-circle-info fa-xl", function (btn, map) {
-  $("#exampleModal").modal("show");
+var infoBtn = L.easyButton('<i class="fa-solid fa-circle-info fa-xl"></i>', function (btn, map) {
+  $("#countryInfoModal").modal("show");
 });
 
 // ---------------------------------------------------------
@@ -38,7 +44,19 @@ $(document).ready(function () {
   map = L.map("map", {
     layers: [streets]
   }).setView([54.5, -4], 6);
-  
+
+  if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) { // Check if geolocation is available in the browser
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude; // Get the user's latitude and longitude
+        map.setView([lat, lng], 10); // Set the map view to the user's location with a zoom level of 10
+      }, function(error) {
+        console.error("Error getting user location:", error);
+      });
+    } else {
+      console.log("Geolocation is not available in this browser.");
+    }
+
   // setView is not required in your application as you will be
   // deploying map.fitBounds() on the country border polygon
 
@@ -52,17 +70,22 @@ $(document).ready(function () {
 $("#countrySelect").click(function () {
   $.ajax({
     url: '../libs/php/getCountry.php', 
-    type: 'POST',
+    type: 'GET',
     dataType: 'json',
-    success: function(response) {
-      console.log(response); // Logs the data received from the server to the console
+      success: function(response) {
+      if (response.status.code === "200") {
+        let dropDown = $('#countrySelect'); // Select the dropdown element by its ID
+        dropDown.empty(); // Clear any existing options in the dropdown
+        dropDown.append('<option value="" disabled selected>Select a Country</option>'); // Append a default option to prompt the user to select a country
 
-      let dropDown = $('#countrySelect'); //Variable created for the dropdown box
-      dropDown.empty(); //Prevents duplicate entries in the dropdown box
-
-      response.data.forEach(country => { //Loops through each country in the data received.
-        dropDown.append(`<option value="${country.iso_a2}">${country.name}</option>`);
-      }); //Appends the country name and iso code to the dropdown box
+        // Access the 'data' property of the response
+        response.data.forEach(country => { // Loop through each country in the data array
+          dropDown.append(`<option value="${country.iso_a2}">${country.name}</option>`); // Append an option element to the dropdown for each country, using the ISO code as the value and the country name as the display text
+        });
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("Error: " + textStatus);
     }
   });
 });
